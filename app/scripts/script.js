4762e8061c7d361ca01b4293688f8978ec65b59b
Margo <luckinamargo@gmail.com>
2017-08-14 11:22:52 +0700

    sorting by name

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,12 +1,24 @@
 new Vue({
     el: '#app',
     data: {
-        list: false
+        list: false,
+        sortKey: '№',
+        search: '',
+        reverse: false,
+        columns: ['№', 'Product number', 'Date', 'Category', 'Name', 'Quantity'],
     },
     mounted: function () {
         this.load();
     },
     methods: {
+        sortBy: function(sortKey) {
+            this.list.sort(this.compareByName);
+        },
+        compareByName: function (a, b) {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        },
         load: function () {
 
             this.$http.get('api/list.json').then(response => {
